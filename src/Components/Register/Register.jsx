a2a750a6de346bed00c8268044c6c712d008345f
Ornella B <borges.ornella@gmail.com>
2023-09-13 10:16:01 +0200

    init components

---
+++ src/Components/Register/Register.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Register = () => {
+    return (
+        <div className="container">
+            <p>Register</p>
+        </div>
+    );
+};
+
+export default Register;
