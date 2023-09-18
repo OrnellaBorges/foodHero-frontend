a9f4a5c8b3485d7f6fd4a96e4f723b44e78b6109
Ornella B <borges.ornella@gmail.com>
2023-09-18 15:43:11 +0200

    create axios call to register Components

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+//import Sidebar from "./SideBar Section";
+//import Sidebar from "./SideBar Section";
 
 const Dashboard = () => {
     return (
--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 //import "../index.scss";
 //import "./Login.css";
+
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import Axios from "axios";
+
+//import './Register.css'
+//import './App.css'
+
 import "../Login/Login.scss";
 
 //import Icons
@@ -16,6 +23,40 @@ import { MdMarkEmailRead } from "react-icons/md";
 //import { Logo } from "../../assets/vite.svg";
 
 const Register = () => {
+    //Need UseState to hold our inputs = memorisation des inputs
+    const [email, setEmail] = useState("");
+    const [userName, setUserName] = useState("");
+    const [password, setPassword] = useState("");
+
+    const [isError, setIsError] = useState(false);
+
+    // Stockage de l'url
+    const apiUrl = "http://localhost:9600";
+
+    //ONCLICK Variable
+    const createUser = () => {
+        // ici on met un fetch qui va permettre de faire appel a la route d'enregistrement d'un utilisateur
+        // mais on a a besoin de la librairie axios
+
+        // INFO > on peut creer les axios dans une autre page a part mais on le garde ici pour l'instant
+        Axios.post(`${apiUrl}/create`, {
+            Email: email,
+            UserName: userName,
+            Password: password,
+        })
+            //on utilise le .then en cas de succes il utilise cette methode et on passe en argument
+            .then(() => {
+                console.log("User has been created");
+            })
+
+            // en cas d' erreur ça passe dans le catch et ça renvoit
+            .catch(() => {
+                // on cree un state error au dessus pour l'initialiser a false au départ
+                // lorsqu'il y aura un soucis celui- ci permettra de l'utiliser afin de renvoyer un composant d'avertissement à l'utilisateur afin d'avertur qu'il y a eu un soucis
+                setIsError(true);
+            });
+    };
+
     return (
         <div className="loginPage flex ">
             <div className="container">
@@ -32,9 +73,15 @@ const Register = () => {
                         <h3>HI !</h3>
                     </div>
                     <form className="form grid" name="loginForm">
-                        <span className="showMessage">
-                            Register Status will go here
-                        </span>
+                        {/* // INFO > En cas de soucis on fait un ternaire qui
+                        renvoi le msg d'erreur en rouge si ça rentre dans le
+                        catch de l'axios au dessus comme isError sera a true*/}
+                        {isError ? (
+                            <p className="showMessage errorMessage">
+                                La création de votre compte a echoué. Veuillez
+                                essayer ultérieurement.
+                            </p>
+                        ) : null}
                         <div className="inputDiv">
                             <label htmlFor="email">Email</label>
                             <div className="input flex"></div>
@@ -42,10 +89,13 @@ const Register = () => {
                             <input
                                 type="email"
                                 id="email"
-                                placeholder="Enter Username"
+                                placeholder="Enter Your Email"
+                                onChange={(event) => {
+                                    setEmail(event.target.value);
+                                }}
                             />
                         </div>
-                        <div className="inputDiv">
+                        <div className="inputDigv">
                             <label htmlFor="username">Username</label>
                             <div className="input flex"></div>
                             <FaUserShield className="icon" />
@@ -53,6 +103,9 @@ const Register = () => {
                                 type="text"
                                 id="email"
                                 placeholder="Enter Username"
+                                onChange={(event) => {
+                                    setUserName(event.target.value);
+                                }}
                             />
                         </div>
                         <div className="inputDiv">
@@ -63,16 +116,21 @@ const Register = () => {
                                 type="text"
                                 id="passeword"
                                 placeholder="Enter Password"
+                                onChange={(event) => {
+                                    setPassword(event.target.value);
+                                }}
                             />
                         </div>
-                        <button type="submit" className="btn flex">
+                        <button
+                            type="submit"
+                            className="btn flex"
+                            onClick={createUser}
+                        >
                             <span>Register</span>
                             <AiOutlineSwapRight className="icon" />
                         </button>
-
-                        <span className="forgotPassword">
-                            Forgot your password
-                        </span>
+                        {/* //INFO > Faire une route forgot dans le back */}
+                        <p className="forgotPassword">Forgot your password</p>
                         <a href="">Click here</a>
                     </form>
                 </div>
