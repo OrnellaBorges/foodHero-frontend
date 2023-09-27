f0937ea73d3292ca535491595369795325cf0bfd
Ornella B <borges.ornella@gmail.com>
2023-09-27 10:18:18 +0200

    save

--- src/Components/Dashboard/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Components/Dashboard/Dashboard.jsx
@@ -10,10 +10,10 @@ const Dashboard = () => {
     return (
         <div className="dashboard flex">
             <div className="dashboardContainer flex"></div>
-            <h1>DASHBOARD PAGE</h1>
+            {/* <h1>DASHBOARD PAGE</h1> */}
             <Sidebar />
             <Body />
-            <a href="/">Log Out</a>
+            {/* <a href="/">Log Out</a> */}
         </div>
     );
 };
--- src/Components/Dashboard/Components/SideBar Section/Sidebar.jsx
+++ src/Components/Dashboard/Components/SideBar Section/Sidebar.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 
+//IMPORT ICONS FROM REACT ICONS
+import { IoMdSpeedometer } from "react-icons/io";
+import { MdDeliveryDining } from "react-icons/md";
+import { MdOutlineExplore } from "react-icons/md";
+import { BsTrophy } from "react-icons/bs";
+
 //import style
 import "./sidebar.css";
+import "../../../../index.scss";
 
-//import assets
+//import assets and images
 import logo from "../../../../assets/logo.png";
 
 const Sidebar = () => {
@@ -13,6 +20,66 @@ const Sidebar = () => {
                 <img src={logo} alt="logo" />
             </div>
             <h2 className="">FOODHERO</h2>
+
+            <div className="menuDiv">
+                <h3 className="divTitle">QUICK MENU</h3>
+                <ul className="menuList grid">
+                    <li className="listItem">
+                        <a href="#" className="menuLink flex">
+                            <IoMdSpeedometer className="icon" />
+                            <span className="smallText">Dash board</span>
+                        </a>
+                    </li>
+                    <li className="listItem">
+                        <a href="#" className="menuLink flex">
+                            <MdDeliveryDining className="icon" />
+                            <span className="smallText">My Orders</span>
+                        </a>
+                    </li>
+                    <li className="listItem">
+                        <a href="#" className="menuLink flex">
+                            <MdOutlineExplore className="icon" />
+                            <span className="smallText">Explore</span>
+                        </a>
+                    </li>
+                    <li className="listItem">
+                        <a href="#" className="menuLink flex">
+                            <BsTrophy className="icon" />
+                            <span className="smallText">Products</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+
+            <div className="settingsDiv">
+                <h3 className="divTitle">SETTINGS</h3>
+                <ul className="menuList grid">
+                    <li className="listItem">
+                        <a href="#" className="menuLink flex">
+                            <AiOutlinePieChart className="icon" />
+                            <span className="smallText">Dash board</span>
+                        </a>
+                    </li>
+                    <li className="listItem">
+                        <a href="#" className="menuLink flex">
+                            <MdDeliveryDining className="icon" />
+                            <span className="smallText">My Orders</span>
+                        </a>
+                    </li>
+                    <li className="listItem">
+                        <a href="#" className="menuLink flex">
+                            <MdOutlineExplore className="icon" />
+                            <span className="smallText">Explore</span>
+                        </a>
+                    </li>
+                    <li className="listItem">
+                        <a href="#" className="menuLink flex">
+                            <BsTrophy className="icon" />
+                            <span className="smallText">Products</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
         </div>
     );
 };
