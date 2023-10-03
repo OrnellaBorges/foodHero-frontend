ba546b35ae4c9210976af6287ecb35aaa24795ba
Ornella B <borges.ornella@gmail.com>
2023-10-03 12:02:01 +0200

    init activity section

--- src/Components/Dashboard/Components/ActivitySection/Activity.jsx
+++ src/Components/Dashboard/Components/ActivitySection/Activity.jsx
@@ -1,7 +1,33 @@
 import React from "react";
 
+// import icons
+import { BsArrowRightShort } from "react-icons/bs";
+
+//import images
+import mikael from "../../../../assets/mikael.jpg";
+
 const Activity = () => {
-    return <div>Activity</div>;
+    return (
+        <div className="activitySection">
+            <div className="heading flex">
+                <h1>Recent Activity</h1>
+                <button className="btn flex">
+                    See All
+                    <BsArrowRightShort className="icon" />
+                </button>
+            </div>
+            <div className="secContainer grid">
+                <div className="singleCustomer flex">
+                    <img src={mikael} alt="customer image" />
+                    <div className="customerDetails">
+                        <span className="name"> Ola Mikael</span>
+                        <small>Ordered a new recipe</small>
+                    </div>
+                    <div className="duration">2 min ago</div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Activity;
