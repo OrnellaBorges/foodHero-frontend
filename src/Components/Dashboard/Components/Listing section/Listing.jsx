24d41d94210c2ac226d8919a202b8190c0ddcb43
Ornella B <borges.ornella@gmail.com>
2023-10-05 21:04:41 +0200

    media queries completed for dashbord

--- src/Components/Dashboard/Components/Listing section/Listing.jsx
+++ src/Components/Dashboard/Components/Listing section/Listing.jsx
@@ -7,79 +7,97 @@ import pasta from "../../../../assets/pasta.jpg";
 import user from "../../../../assets/avatar3.jpg";
 
 const Listing = () => {
-    return (
-        <div className="listingSection">
-            <div className="heading flex">
-                <h1>My Listing</h1>
-                <button className="btn flex">
-                    See All
-                    <BsArrowBarRight className="icon" />
-                </button>
-            </div>
-            <div className="secContainer flex">
-                <div className="singleItem">
-                    <AiFillHeart className="icon" />
-                    <AiOutlineHeart className="icon" />
-                    <img src={pasta} alt="image name" />
-                    <h3>PASTA</h3>
-                </div>
-            </div>
-            <div className="sellers flex">
-                <div className="topSellers">
-                    <div className="heading flex">
-                        <h3>Top Sellers</h3>
-                        <button className="btn flex">
-                            See All
-                            <BsArrowBarRight className="icon" />
-                        </button>
-                    </div>
-                    <div className="card flex">
-                        <div className="users">
-                            <img src={user} alt="user image" />
-                            <img src={user} alt="user image" />
-                            <img src={user} alt="user image" />
-                            <img src={user} alt="user image" />
-                        </div>
-                        <div className="cardText">
-                            <span>
-                                Lorem ipsum dolor sit amet consectetur. <br />
-                                <small>
-                                    21 Sellers
-                                    <span className="date">7 Days</span>
-                                </small>
-                            </span>
-                        </div>
-                    </div>
-                </div>
-                <div className="featuredSellers">
-                    <div className="heading flex">
-                        <h3>Featured Sellers</h3>
-                        <button className="btn flex">
-                            See All
-                            <BsArrowBarRight className="icon" />
-                        </button>
-                    </div>
-                    <div className="card flex">
-                        <div className="users">
-                            <img src={user} alt="user image" />
-                            <img src={user} alt="user image" />
-                            <img src={user} alt="user image" />
-                            <img src={user} alt="user image" />
-                        </div>
-                        <div className="cardText">
-                            <span>
-                                Lorem ipsum dolor sit amet consectetur. <br />
-                                <small>
-                                    21 Sellers
-                                    <span className="date">7 Days</span>
-                                </small>
-                            </span>
-                        </div>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="listingSection">
+      <div className="heading flex">
+        <h1>My Listing</h1>
+        <button className="btn flex">
+          See All
+          <BsArrowBarRight className="icon" />
+        </button>
+      </div>
+      <div className="secContainer flex">
+        <div className="singleItem">
+          <AiFillHeart className="icon" />
+          <AiOutlineHeart className="icon" />
+          <img src={pasta} alt="image name" />
+          <h3>PASTA</h3>
         </div>
-    );
+        <div className="singleItem">
+          <AiFillHeart className="icon" />
+          <AiOutlineHeart className="icon" />
+          <img src={pasta} alt="image name" />
+          <h3>PASTA</h3>
+        </div>
+        <div className="singleItem">
+          <AiFillHeart className="icon" />
+          <AiOutlineHeart className="icon" />
+          <img src={pasta} alt="image name" />
+          <h3>PASTA</h3>
+        </div>
+        <div className="singleItem">
+          <AiFillHeart className="icon" />
+          <AiOutlineHeart className="icon" />
+          <img src={pasta} alt="image name" />
+          <h3>PASTA</h3>
+        </div>
+      </div>
+      <div className="sellers flex">
+        <div className="topSellers">
+          <div className="heading flex">
+            <h3>Top Sellers</h3>
+            <button className="btn flex">
+              See All
+              <BsArrowBarRight className="icon" />
+            </button>
+          </div>
+          <div className="card flex">
+            <div className="users">
+              <img src={user} alt="user image" />
+              <img src={user} alt="user image" />
+              <img src={user} alt="user image" />
+              <img src={user} alt="user image" />
+            </div>
+            <div className="cardText">
+              <span>
+                Lorem ipsum dolor sit amet consectetur. <br />
+                <small>
+                  21 Sellers
+                  <span className="date">7 Days</span>
+                </small>
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="featuredSellers">
+          <div className="heading flex">
+            <h3>Featured Sellers</h3>
+            <button className="btn flex">
+              See All
+              <BsArrowBarRight className="icon" />
+            </button>
+          </div>
+          <div className="card flex">
+            <div className="users">
+              <img src={user} alt="user image" />
+              <img src={user} alt="user image" />
+              <img src={user} alt="user image" />
+              <img src={user} alt="user image" />
+            </div>
+            <div className="cardText">
+              <span>
+                Lorem ipsum dolor sit amet consectetur. <br />
+                <small>
+                  21 Sellers
+                  <span className="date">7 Days</span>
+                </small>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Listing;
--- src/Components/Dashboard/Components/Top section/Top.jsx
+++ src/Components/Dashboard/Components/Top section/Top.jsx
@@ -17,77 +17,77 @@ import { LuConciergeBell } from "react-icons/lu";
 //import { IoNotificationsCircleSharp } from "react-icons/io";
 
 const Top = () => {
-    return (
-        <div className="topSection">
-            <div className="headerSection">
-                <div className="title">
-                    <h1>Welcome to FoodHero</h1>
-                    <p>Hello Ornella</p>
-                </div>
-                <div className="searchBar flex">
-                    <input type="text" placeholder="Search" />
-                    <BiSearch className="icon" />
-                </div>
-
-                <div className="adminDiv flex">
-                    <TbMessageCircle2Filled className="icon" />
-                    <LuConciergeBell className="icon" />
-
-                    <div className="adminImage flex">
-                        <img src={user} alt="adminImage" />
-                    </div>
-                </div>
-            </div>
-            <div className="cardSection flex">
-                <div className="rightCard flex">
-                    <h1>Au four et au moulin.</h1>
-                    <p>Faites bouillir la marmite!.</p>
-                    <div className="buttons flex">
-                        <button className="btn">Explore</button>
-                        <button className="btn transparent">Top Sellers</button>
-                    </div>
-                    <div className="videoDiv">
-                        <video src={video} autoPlay loop muted></video>
-                    </div>
-                </div>
-                <div className="leftCard flex">
-                    <div className="main flex">
-                        <div className="textDiv">
-                            <h1>My Stat</h1>
-                            <div className="flex">
-                                <span>
-                                    Today <br /> <small>4 Orders</small>
-                                </span>
-                                <span>
-                                    This Month <br /> <small>127 Orders</small>
-                                </span>
-                                <span className="flex link">
-                                    Go to my orders
-                                    <BsArrowRightShort className="icon" />
-                                </span>
-                            </div>
-                        </div>
-
-                        <div className="imgDiv">
-                            <img src={user} alt="Image name" />
-                        </div>
-                    </div>
-
-                    <div className="sidebarCard">
-                        <BsQuestionCircle className="icon" />
-                        <div className="cardContent">
-                            <div className="circle1"></div>
-                            <div className="circle2"></div>
-
-                            <h3>Help Center</h3>
-                            <p>Having trouble in Planti, contact us.</p>
-                            <button className="btn">Go to help center</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="topSection">
+      <div className="headerSection">
+        <div className="title">
+          <h1>Welcome to FoodHero</h1>
+          <p>Hello Ornella</p>
         </div>
-    );
+        <div className="searchBar flex">
+          <input type="text" placeholder="Search" />
+          <BiSearch className="icon" />
+        </div>
+
+        <div className="adminDiv flex">
+          <TbMessageCircle2Filled className="icon" />
+          <LuConciergeBell className="icon" />
+
+          <div className="adminImage flex">
+            <img src={user} alt="adminImage" />
+          </div>
+        </div>
+      </div>
+      <div className="cardSection flex">
+        <div className="rightCard flex">
+          <h1>Au four et au moulin.</h1>
+          <p>Faites bouillir la marmite!.</p>
+          <div className="buttons flex">
+            <button className="btn">Explore</button>
+            <button className="btn transparent">Top Sellers</button>
+          </div>
+          <div className="videoDiv">
+            <video src={video} autoPlay loop muted></video>
+          </div>
+        </div>
+        <div className="leftCard flex">
+          <div className="main flex">
+            <div className="textDiv">
+              <h1>My Stat</h1>
+              <div className="flex">
+                <span>
+                  Today <br /> <small>4 Orders</small>
+                </span>
+                <span>
+                  This Month <br /> <small>127 Orders</small>
+                </span>
+                <span className="flex link">
+                  Go to my orders
+                  <BsArrowRightShort className="icon" />
+                </span>
+              </div>
+            </div>
+
+            <div className="imgDiv">
+              <img src={user} alt="Image name" />
+            </div>
+          </div>
+
+          <div className="helpCard">
+            <BsQuestionCircle className="icon" />
+            <div className="cardContent">
+              <div className="circle1"></div>
+              <div className="circle2"></div>
+
+              <h3>Help Center</h3>
+              <p>Having trouble in Planti, contact us.</p>
+              <button className="btn">Go to help center</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Top;
