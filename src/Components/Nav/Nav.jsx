be6b5d2b6a6f66466d16af2de387824d53badb28
Ornella B. <borges.ornella@gmail.com>
2023-10-09 22:21:33 +0200

    save work

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -13,83 +13,88 @@ import logo from "../../assets/logo2.png";
 import "./nav.css";
 
 //navbar event
-//const [isActiv, setIsActiv] = useState("menuList");
-const [isClosed, setIsClosed] = useState("navMenuDiv");
-
-const showNav = () => {
-  console.log("first");
-  setIsActiv("menuList activNav");
-};
-
-const handleClickClose = () => {
-  console.log("je suis fermé");
-  setIsClosed("navMenuDiv activNav ");
-};
 
 const Nav = () => {
-  return (
-    <nav className={isClosed}>
-      <ul className="menuList leftMenu flex">
-        <li className="closeNav navItem" onClick={() => handleClickClose()}>
-          <AiFillCloseCircle className="icon" />
-        </li>
+    //const [isActiv, setIsActiv] = useState("menuList");
+    const [isMenuOpen, setIsMenuOpen] = useState(true);
 
-        <li className="navItem">
-          <Link to="/" className="menuLink">
-            {/* <BsFillHouseDoorFill className="icon" /> */}
-            Home
-          </Link>
-        </li>
+    return (
+        <nav className="navMenuDiv">
+            <ul
+                className={`menuList leftMenu flex ${
+                    isMenuOpen ? "" : "closedNav"
+                }`}
+            >
+                <li
+                    className="closeNav navItem"
+                    onClick={() => setIsMenuOpen(false)}
+                >
+                    <AiFillCloseCircle className="icon" />
+                </li>
 
-        <li className="navItem">
-          <Link to="/register" className="menuLink">
-            S'enregistrer
-          </Link>
-        </li>
-        <li className="navItem">
-          <Link to="/login" className="menuLink">
-            Se connecter
-          </Link>
-        </li>
-      </ul>
+                <li className="navItem">
+                    <Link to="/" className="menuLink">
+                        {/* <BsFillHouseDoorFill className="icon" /> */}
+                        Home
+                    </Link>
+                </li>
 
-      <ul className="menuList centerMenu flex">
-        <li className="navItem">
-          <Link to="/" className="menuLink">
-            <div className="logo-container">
-              <img src={logo} className="logo" />
-              <p className="brand">FOODHERO</p>
-            </div>
-          </Link>
-        </li>
+                <li className="navItem">
+                    <Link to="/register" className="menuLink">
+                        S'enregistrer
+                    </Link>
+                </li>
+                <li className="navItem">
+                    <Link to="/login" className="menuLink">
+                        Se connecter
+                    </Link>
+                </li>
+            </ul>
 
-        <li className="menuIcon navItem" onClick={() => showNav()}>
-          <BsFillGrid3X3GapFill className="icon" />
-        </li>
-      </ul>
+            <ul className="menuList centerMenu flex">
+                <li className="navItem">
+                    <Link to="/" className="menuLink">
+                        <div className="logo-container">
+                            <img src={logo} className="logo" />
+                            <p className="brand">FOODHERO</p>
+                        </div>
+                    </Link>
+                </li>
 
-      {/* cette partie devra etre un ternaire si l'user est connecté ou pas voir beer4you header */}
+                <li
+                    className="menuIcon navItem"
+                    onClick={() => setIsMenuOpen(true)}
+                >
+                    <BsFillGrid3X3GapFill className="icon" />
+                </li>
+            </ul>
 
-      <ul className="menuList rightMenu flex">
-        <li className="navItem">
-          <Link to="/dashboard" className="menuLink">
-            Dashboard
-          </Link>
-        </li>
-        <li className="navItem">
-          <Link to="/account" className="menuLink">
-            Mon compte
-          </Link>
-        </li>
-        <li className="navItem">
-          <Link to="/basket" className="menuLink">
-            {/* <BiSolidCart className="icon" /> */}
-            Panier
-          </Link>
-        </li>
-      </ul>
-    </nav>
-  );
+            {/* cette partie devra etre un ternaire si l'user est connecté ou pas voir beer4you header */}
+
+            <ul
+                className={`menuList rightMenu flex ${
+                    isMenuOpen ? "" : "closedNav"
+                }`}
+            >
+                <li className="navItem">
+                    <Link to="/dashboard" className="menuLink">
+                        Dashboard
+                    </Link>
+                </li>
+                <li className="navItem">
+                    <Link to="/account" className="menuLink">
+                        Mon compte
+                    </Link>
+                </li>
+                <li className="navItem">
+                    <Link to="/basket" className="menuLink">
+                        {/* <BiSolidCart className="icon" /> */}
+                        Panier
+                    </Link>
+                </li>
+            </ul>
+        </nav>
+    );
 };
 
 export default Nav;
