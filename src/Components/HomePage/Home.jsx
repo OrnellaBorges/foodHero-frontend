b7750dbea16782ce3f925a74b938deed745f203a
Ornella B <borges.ornella@gmail.com>
2023-11-02 14:05:45 +0100

    save

--- src/Components/HomePage/Home.jsx
+++ src/Components/HomePage/Home.jsx
@@ -3,21 +3,20 @@ import Hero from "./Hero/Hero";
 
 import AllAdsCards from "../AdsCards/AllAdsCards";
 import Rgpd from "../Concent/RgpdBanner";
-//import Cookie from "../Cookie/Cookie";
 
 import { useSelector, useDispatch } from "react-redux";
 import { selectUser, connectUser } from "../../slices/userSlice";
 
 const Home = (props) => {
-  const user = useSelector(selectUser);
+    const user = useSelector(selectUser);
 
-  return (
-    <div className="hp">
-      <Hero />
-      <Rgpd />
-      <AllAdsCards />
-    </div>
-  );
+    return (
+        <div className="hp">
+            <Hero />
+            <Rgpd />
+            <AllAdsCards />
+        </div>
+    );
 };
 
 export default Home;
